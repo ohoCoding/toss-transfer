efb135d925d3076ef7abe7f8f8d7cb0a19f4b015
ohoCoding <youngho7482@gmail.com>
2025-02-11 12:51:17 +0900

    feat: :sparkles: 로딩 추가

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+
+const Loading = () => (
+  <div className="flex-1 flex items-center justify-center">
+    <FontAwesomeIcon width={30} color="gray" icon={faSpinner} spin size="2xl" />
+  </div>
+);
+
+export default Loading;
