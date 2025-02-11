6f7abeae5113b81f5c0edb60de4c423371c37a4f
ohoCoding <youngho7482@gmail.com>
2025-02-11 12:51:32 +0900

    feat: :sparkles: 계좌받는 컴포넌트 추가

---
+++ src/components/main/Accounts.tsx
@@ -0,0 +1,13 @@
+'use client';
+const Accounts = () => {
+  return (
+    <div>
+      <h1 className="flex justify-between font-bold items-center mb-5">
+        <span className="text-lg text-black">계좌</span>
+        <span className="total-balance text-sm text-gray-500">원</span>
+      </h1>
+    </div>
+  );
+};
+
+export default Accounts;
