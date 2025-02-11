828ad357a23e0b0697eac8bc80a938b133b11c38
ohoCoding <youngho7482@gmail.com>
2025-02-11 18:42:24 +0900

    feat: :sparkles: 송금 버튼 컴포넌트

---
+++ src/components/main/AccountLink.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import useStore from '@/store';
+import Link from 'next/link';
+
+interface AccountLinkProps {
+  accountId: number;
+  account: string;
+  balance: number;
+}
+
+const AccountLink = ({ accountId, account, balance }: AccountLinkProps) => {
+  const { saveAccountId, setSource } = useStore();
+
+  const handleClick = () => {
+    saveAccountId(accountId);
+    setSource(account, balance);
+  };
+
+  return (
+    <div className="h-8">
+      <Link
+        className="h-full inline-flex items-center px-[12px] text-sm rounded-sm bg-gray-100 hover:bg-blue-500 hover:text-white transition-colors cursor-pointer"
+        href={`/remit`}
+        onClick={handleClick}
+      >
+        송금
+      </Link>
+    </div>
+  );
+};
+
+export default AccountLink;
