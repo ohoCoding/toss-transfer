9a6069dac810fa0abafebfb8c72872c6905ac864
ohoCoding <youngho7482@gmail.com>
2025-02-11 12:50:46 +0900

    feat: :sparkles: zustand 활용 state 초기화

---
+++ src/store/index.ts
@@ -0,0 +1,41 @@
+import { create } from 'zustand';
+
+interface State {
+  accountId: number;
+  money: number;
+
+  source: {
+    account: string;
+    balance: number;
+  };
+
+  target: {
+    id: number;
+    name: string;
+    account: string;
+    type: string;
+  };
+}
+
+const init = {
+  accountId: 0,
+  money: 0,
+
+  source: {
+    account: '',
+    balance: 0,
+  },
+
+  target: {
+    id: 0,
+    name: '',
+    account: '',
+    type: '',
+  },
+};
+
+const useStore = create<State>()((set) => ({
+  ...init,
+}));
+
+export default useStore;
